c5be27039de073f0b3ba55811fc04877c90325d1
yi.zheng@showself.com <yi.zheng@showself.com>
2020-09-25 11:43:52 +0800

    add solution LCP 18

--- Play-with-Alorithms/LeetCode/Solution_01.swift
+++ Play-with-Alorithms/LeetCode/Solution_01.swift
@@ -1668,7 +1668,7 @@ extension Solution_01 {
             return nil
         }
         var sum = 0
-        func dfs(_ node : TreeNode<Int>?) {
+        func dfs(_ node: TreeNode<Int>?) {
             guard let node = node else {
                 return
             }
@@ -1681,4 +1681,114 @@ extension Solution_01 {
         return root
     }
 
+    // MARK: - ------------- 早餐组合 leetCode #LCP 18
+
+    /*
+     https://leetcode-cn.com/problems/2vYnGI/
+     小扣在秋日市集选择了一家早餐摊位，一维整型数组 staple 中记录了每种主食的价格，
+     一维整型数组 drinks 中记录了每种饮料的价格。小扣的计划选择一份主食和一款饮料，
+     且花费不超过 x 元。请返回小扣共有多少种购买方案。
+
+     注意：答案需要以 1e9 + 7 (1000000007) 为底取模，如：计算初始结果为：1000000008，请返回 1
+
+     示例 1：
+     输入：staple = [10,20,5], drinks = [5,5,2], x = 15
+
+     输出：6
+     解释：小扣有 6 种购买方案，所选主食与所选饮料在数组中对应的下标分别是：
+     第 1 种方案：staple[0] + drinks[0] = 10 + 5 = 15；
+     第 2 种方案：staple[0] + drinks[1] = 10 + 5 = 15；
+     第 3 种方案：staple[0] + drinks[2] = 10 + 2 = 12；
+     第 4 种方案：staple[2] + drinks[0] = 5 + 5 = 10；
+     第 5 种方案：staple[2] + drinks[1] = 5 + 5 = 10；
+     第 6 种方案：staple[2] + drinks[2] = 5 + 2 = 7。
+
+     示例 2：
+     输入：staple = [2,1,1], drinks = [8,9,5,1], x = 9
+
+     输出：8
+     解释：小扣有 8 种购买方案，所选主食与所选饮料在数组中对应的下标分别是：
+     第 1 种方案：staple[0] + drinks[2] = 2 + 5 = 7；
+     第 2 种方案：staple[0] + drinks[3] = 2 + 1 = 3；
+     第 3 种方案：staple[1] + drinks[0] = 1 + 8 = 9；
+     第 4 种方案：staple[1] + drinks[2] = 1 + 5 = 6；
+     第 5 种方案：staple[1] + drinks[3] = 1 + 1 = 2；
+     第 6 种方案：staple[2] + drinks[0] = 1 + 8 = 9；
+     第 7 种方案：staple[2] + drinks[2] = 1 + 5 = 6；
+     第 8 种方案：staple[2] + drinks[3] = 1 + 1 = 2；
+
+     提示：
+     1 <= staple.length <= 10^5
+     1 <= drinks.length <= 10^5
+     1 <= staple[i],drinks[i] <= 10^5
+     1 <= x <= 2*10^5
+     */
+    /*
+     先对两个数组从小到大进行排序
+     双指针 i 和 j 分别指向 staple 的头部和 drink 的尾部
+     i 从小到大移动，j从大到小移动，知道找到 staple[i] + drinks[j] <= x 的j位置，那么 j 前面的（包括j自己）就都是有效的搭配
+     如果 j 走到头了（j==-1），即没有 staple[i] + drinks[j] <= x 的搭配，那么i往后的元素都是无效的了，因为 staple[i+1] > staple[i]
+     所以，从i+1开始的往后的所有元素都不可能配对成功
+     https://leetcode-cn.com/problems/2vYnGI/
+     */
+    func breakfastNumber(_ staple: [Int], _ drinks: [Int], _ x: Int) -> Int {
+        var res = 0
+        let staple = staple.sorted()
+        let drinks = drinks.sorted()
+        let p = 1000000007
+        var j = drinks.count - 1
+        for s in staple {
+            if s >= x {
+                break
+            }
+            while j >= 0 && s + drinks[j] > x  {
+                j -= 1
+            }
+            if j == -1 { // j 走到头了
+                break
+            }
+            // 如果这里直接 (res + j + 1) % 1000000007， 会超时，所以这里预先定义变量 p 存储 1000000007
+            res = (res + j + 1) % p
+        }
+        return res
+    }
+
+    // 超时了
+    func breakfastNumber_1(_ staple: [Int], _ drinks: [Int], _ x: Int) -> Int {
+        var res = 0
+        let mod: Int = 1000000007
+        let staple = staple.sorted()
+        let drinks = drinks.sorted()
+        for i in 0 ..< staple.count {
+            var j = drinks.count - 1
+            let t = x - staple[i]
+            while j >= 0 && drinks[j] > t {
+                j -= 1
+            }
+            if j == -1 { // j 走到头了
+                break
+            }
+            res += (j + 1)
+            res %= mod
+        }
+        return res
+    }
+
+    // 超时了
+    func breakfastNumber_2(_ staple: [Int], _ drinks: [Int], _ x: Int) -> Int {
+        var res = 0
+        for i in 0 ..< staple.count {
+            let c = staple[i]
+            if c >= x {
+                continue
+            }
+            for j in 0 ..< drinks.count {
+                if drinks[j] > (x - c) {
+                    continue
+                }
+                res += 1
+            }
+        }
+        return res
+    }
 }
